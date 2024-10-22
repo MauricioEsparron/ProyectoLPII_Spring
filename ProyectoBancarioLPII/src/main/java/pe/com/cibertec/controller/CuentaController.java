e15f510257ed68b2737ba62d593430a1b8be98e3
MauricioEsparron <mauricio.resparron@gmail.com>
2024-10-22 03:19:57 -0500

    los listados principales funcionan (aun no hay spring security ni auditoria)

--- ProyectoBancarioLPII/src/main/java/pe/com/cibertec/controller/CuentaController.java
+++ ProyectoBancarioLPII/src/main/java/pe/com/cibertec/controller/CuentaController.java
@@ -11,57 +11,62 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pe.com.cibertec.model.CuentaEntity;
+import pe.com.cibertec.model.TipoCuentaEntity;
 import pe.com.cibertec.service.CuentaService;
+import pe.com.cibertec.service.TipoCuentaService;
 
 @Controller
-@RequestMapping("/cuentas") 
+@RequestMapping("/cuentas")
 public class CuentaController {
 
-    @Autowired
-    private CuentaService cuentaService;
-    
-    @GetMapping("/")
-    public String listarCuentas(Model model) {
-        List<CuentaEntity> listaCuentas = cuentaService.buscarCuentas();
-        model.addAttribute("lista_cuentas", listaCuentas);
-        return "listar_cuentas";
-    }
-    
-    @GetMapping("/registrar_cuenta")
-    public String mostrarRegistrarCuenta(Model model) {
-        model.addAttribute("cuenta", new CuentaEntity());
-        return "registrar_cuenta";
-    }
-    
-    @PostMapping("/registrar_cuenta")
-    public String registrarCuenta(@ModelAttribute("cuenta") CuentaEntity cuenta, Model model) {
-        cuentaService.crearCuenta(cuenta);
-        return "redirect:/cuentas/";
-    }
-    
-    @GetMapping("/detalle_cuenta/{id}")
-    public String verDetalleCuenta(@PathVariable("id") Integer id, Model model) {
-        CuentaEntity cuenta = cuentaService.buscarCuentaPorId(id);
-        model.addAttribute("cuenta", cuenta);
-        return "detalle_cuenta";
-    }
-    
-    @GetMapping("/delete_cuenta/{id}")
-    public String deleteCuenta(@PathVariable("id") Integer id) {
-        cuentaService.eliminarCuenta(id);
-        return "redirect:/cuentas/";
-    }
-    
-    @GetMapping("/editar_cuenta/{id}")
-    public String mostrarActualizarCuenta(@PathVariable("id") Integer id, Model model) {
-        CuentaEntity cuenta = cuentaService.buscarCuentaPorId(id);
-        model.addAttribute("cuenta", cuenta);
-        return "editar_cuenta";
-    }
-    
-    @PostMapping("/editar_cuenta/{id}")
-    public String actualizarCuenta(@PathVariable("id") Integer id, @ModelAttribute("cuenta") CuentaEntity cuenta) {
-        cuentaService.actualizarCuenta(id, cuenta);
-        return "redirect:/cuentas/";
-    }
+	@Autowired
+	private CuentaService cuentaService;
+
+	@Autowired
+	private TipoCuentaService tipoCuentaService;
+
+	@GetMapping("/")
+	public String listarCuentas(Model model) {
+		List<CuentaEntity> listaCuentas = cuentaService.buscarCuentas();
+		model.addAttribute("lista_cuentas", listaCuentas);
+		return "listar_cuentas";
+	}
+
+	@GetMapping("/registrar_cuenta")
+	public String mostrarRegistrarCuenta(Model model) {
+		model.addAttribute("cuenta", new CuentaEntity());
+		return "registrar_cuenta";
+	}
+
+	@PostMapping("/registrar_cuenta")
+	public String registrarCuenta(@ModelAttribute("cuenta") CuentaEntity cuenta, Model model) {
+		cuentaService.crearCuenta(cuenta);
+		return "redirect:/cuentas/";
+	}
+
+	@GetMapping("/detalle_cuenta/{id}")
+	public String verDetalleCuenta(@PathVariable("id") Integer id, Model model) {
+		CuentaEntity cuenta = cuentaService.buscarCuentaPorId(id);
+		model.addAttribute("cuenta", cuenta);
+		return "detalle_cuenta";
+	}
+
+	@GetMapping("/delete_cuenta/{id}")
+	public String deleteCuenta(@PathVariable("id") Integer id) {
+		cuentaService.eliminarCuenta(id);
+		return "redirect:/cuentas/";
+	}
+
+	@GetMapping("/editar_cuenta/{id}")
+	public String mostrarActualizarCuenta(@PathVariable("id") Integer id, Model model) {
+		CuentaEntity cuenta = cuentaService.buscarCuentaPorId(id);
+		model.addAttribute("cuenta", cuenta);
+		return "editar_cuenta";
+	}
+
+	@PostMapping("/editar_cuenta/{id}")
+	public String actualizarCuenta(@PathVariable("id") Integer id, @ModelAttribute("cuenta") CuentaEntity cuenta) {
+		cuentaService.actualizarCuenta(id, cuenta);
+		return "redirect:/cuentas/";
+	}
 }
--- ProyectoBancarioLPII/src/main/java/pe/com/cibertec/controller/LoginController.java
+++ ProyectoBancarioLPII/src/main/java/pe/com/cibertec/controller/LoginController.java
@@ -4,15 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	@PostMapping("/login")
-	public String login(Model model) {
-
-		return "login";
-
+	@GetMapping("/login")
+	public String mostrarLogin(@RequestParam(value = "error", required = false) String error, Model model) {
+		if (error != null) {
+			model.addAttribute("errorMessage", "Usuario o contraseña incorrectos.");
+		}
+		return "login"; // Asegúrate de que este nombre coincide con tu archivo login.html
 	}
 
 	@GetMapping("/logout")
